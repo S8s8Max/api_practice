c2f74b0100a3830e2d60d3b18aa2346f10b0d59a
S8s8Max <k6play7210@gmail.com>
2021-02-07 00:14:06 +0900

    debug

--- backend/settings.py
+++ backend/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+DEBUG = False
+
 try:
     from .local_settings import *
 except ImportError:
     pass
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
