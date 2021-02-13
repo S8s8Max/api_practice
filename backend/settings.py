85d6a5605f6f87c55b915732c9657c4c93b3c4ee
S8s8Max <k6play7210@gmail.com>
2021-02-14 00:55:20 +0900

    created dirctory on heroku bash

--- backend/settings.py
+++ backend/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# 'heroku run bash' でheroku 上にも 'staticfiles' を作る
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
